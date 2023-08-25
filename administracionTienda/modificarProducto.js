37383a577505197042be6a5ee9d15f1c505b89c0
Enrique <enriquetrevejohortet@gmail.com>
2023-08-25 18:50:03 +0200

    Funcionalidad Modificar Productos

--- administracionTienda/modificarProducto.js
+++ administracionTienda/modificarProducto.js
@@ -1,21 +1,21 @@
-// modificarProducto.js
+document.addEventListener("DOMContentLoaded", function() {
+    var enlacesModificar = document.querySelectorAll(".enlaceModificar");
 
-var enlacesModificar = document.querySelectorAll(".enlaceModificar");
-    
-enlacesModificar.forEach(function(enlace) {
-    enlace.addEventListener("click", function(event) {
-        event.preventDefault();
-        var idProducto = this.getAttribute("data-id");
-        mostrarFormularioModificar(idProducto);
+    enlacesModificar.forEach(function(enlace) {
+        enlace.addEventListener("click", function(event) {
+            event.preventDefault(); // Evita que el enlace recargue la página
+            var idProducto = this.getAttribute("data-id");
+            mostrarFormularioModificar(idProducto);
+        });
     });
+
+    function mostrarFormularioModificar(idProducto) {
+        var formulariosModificar = document.querySelectorAll(".formularioModificar");
+        formulariosModificar.forEach(function(formulario) {
+            formulario.style.display = "none";
+        });
+
+        var formularioMostrar = document.getElementById("formularioModificar_" + idProducto);
+        formularioMostrar.style.display = "block";
+    }
 });
-
-function mostrarFormularioModificar(idProducto) {
-    var formulariosModificar = document.querySelectorAll(".formularioModificar");
-    formulariosModificar.forEach(function(formulario) {
-        formulario.style.display = "none";
-    });
-    
-    var formularioMostrar = document.getElementById("formularioModificar_" + idProducto);
-    formularioMostrar.style.display = "block";
-}
--- administracionTienda/stockProductos.php
+++ administracionTienda/stockProductos.php
@@ -7,12 +7,11 @@ include("../verificacionUsuarioConectado.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="hojaEstiloAdmin.css">
+    <link rel="stylesheet" href="hojaEstiloAdmin.css">
     <link rel="stylesheet" href="hojaEstiloAdminProductos.css">
     <title>Panel de Administración</title>
     <script src="productos.js"></script>
     <script src="ocultarStock"></script>
-    <script src="modificarProducto.js"></script>
 </head>
 <body>
     <h1 class="titulo">Alimentación San Enrique <br>ADMINISTRACIÓN</h1>
@@ -38,72 +37,76 @@ include("../verificacionUsuarioConectado.php");
                     <div class="dropdown-content">
                         <a href="javascript:void(0);" onclick="mostrarStockProductos()">Stock productos &nbsp;</a>
                         <a href="javascript:void(0);" onclick="mostrarFormularioAnadir()">Añadir producto &nbsp;</a>
-
                     </div>
                 </div>
         </div>
     <div id="contenidoDinamico">
         <div id="ocultarStock">
         <?php
-    try {
-    // Realiza la consulta para obtener los productos
-    $sql = "SELECT * FROM productos";
-    $result = $pdo->query($sql);
+        try {
+            // Realiza la consulta para obtener los productos
+            $sql = "SELECT * FROM productos";
+            $result = $pdo->query($sql);
 
-    // Verifica si la consulta se ejecutó correctamente
-    if ($result) {
-        // Obtén los resultados de la consulta
-        $resultados = $result->fetchAll(PDO::FETCH_ASSOC);
+            // Verifica si la consulta se ejecutó correctamente
+            if ($result) {
+                // Obtén los resultados de la consulta
+                $resultados = $result->fetchAll(PDO::FETCH_ASSOC);
 
-        // Muestra los productos en una tabla
-        echo "<table>";
-        echo "<tr><th>ID</th><th>Nombre</th><th>Lote</th><th>Proveedor</th><th>Stock</th><th>Precio</th><th>Acciones</th></tr>";
-        foreach ($resultados as $producto) {
-            echo "<tr>";
-            // Mostrar los datos de cada producto
-            echo "<td>" . $producto["id"] . "</td>";
-            echo "<td>" . $producto["nombre"] . "</td>";
-            echo "<td>" . $producto["lote"] . "</td>";
-            echo "<td>" . $producto["proveedor"] . "</td>";
-            echo "<td>" . $producto["stock"] . "</td>";
-            echo "<td>" . $producto["precio"] . '€' . "</td>";
-            // Agregar enlace para eliminar el producto
-    echo '<td><a href="procesarEliminarProducto.php?idProducto=' . $producto["id"] . '">Eliminar</a>
-    <a href="javascript:void(0);" class="enlaceModificar" data-id="' . $producto["id"] . '">Modificar</a></td>';
-echo "</tr>";
+                if (count($resultados) > 0) {
+                    // Muestra los productos en una tabla
+                    echo "<table>";
+                    echo "<tr><th>ID</th><th>Nombre</th><th>Lote</th><th>Proveedor</th><th>Stock</th><th>Precio</th><th>Acciones</th></tr>";
+                    foreach ($resultados as $producto) {
+                        echo "<tr>";
+                        // Mostrar los datos de cada producto
+                        echo "<td>" . $producto["id"] . "</td>";
+                        echo "<td>" . $producto["nombre"] . "</td>";
+                        echo "<td>" . $producto["lote"] . "</td>";
+                        echo "<td>" . $producto["proveedor"] . "</td>";
+                        echo "<td>" . $producto["stock"] . "</td>";
+                        echo "<td>" . $producto["precio"] . '€' . "</td>";
+                        // Agregar enlace para eliminar y modificar el producto
+                        echo '<td><a href="procesarEliminarProducto.php?idProducto=' . $producto["id"] . '">Eliminar</a>';
+                        echo '<div><a href="javascript:void(0);" class="enlaceModificar" data-id="' . $producto["id"] . '">Modificar</a></div></td>';
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "No hay productos disponibles.";
+                }
+            } else {
+                // Si la consulta falló
+                echo "Error al obtener los productos";
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
         }
-        echo "</table>";
-    } else {
-        // Si la consulta falló
-        echo "Error al obtener los productos";
-    }
-
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-?>
-<div class="formularioModificar" id="formularioModificar_<?php echo $producto['id']; ?>" style="display: none;">
-    <h2>Modificar Producto</h2>
-    <form action="procesarModificarProducto.php" method="post">
-        <!-- Campos del formulario con valores pre-cargados -->
-        <input type="hidden" name="idProducto" value="<?php echo $producto['id']; ?>">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" value="<?php echo $producto['nombre']; ?>" required><br>
-        <label for="lote">Lote:</label>
-        <input type="text" name="lote" value="<?php echo $producto['lote']; ?>" required><br>
-        <label for="proveedor">Proveedor:</label>
-        <input type="text" name="proveedor" value="<?php echo $producto['proveedor']; ?>" required><br>
-        <label for="idProveedor">ID del Proveedor:</label>
-        <input type="text" name="idProveedor" value="<?php echo $producto['idProveedor']; ?>" required><br>
-        <label for="stock">Stock:</label>
-        <input type="text" name="stock" value="<?php echo $producto['stock']; ?>" required><br>
-        <label for="precio">Precio:</label>
-        <input type="text" name="precio" value="<?php echo $producto['precio']; ?>" required><br>
-        <button type="submit">Guardar Cambios</button>
-    </form>
-</div>
-
-
+        ?>
+        </div>
+        <!-- Código para el formulario de modificación -->
+<?php foreach ($resultados as $producto) { ?>
+    <div class="formularioModificar" id="formularioModificar_<?php echo $producto['id']; ?>" style="display: none;">
+        <h2>Modificar Producto</h2>
+        <form action="procesarModificarProducto.php" method="post">
+            <!-- Campos del formulario con valores pre-cargados -->
+            <input type="hidden" name="idProducto" value="<?php echo $producto['id']; ?>">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" value="<?php echo $producto['nombre']; ?>" required><br>
+            <label for="lote">Lote:</label>
+            <input type="text" name="lote" value="<?php echo $producto['lote']; ?>" required><br>
+            <label for="proveedor">Proveedor:</label>
+            <input type="text" name="proveedor" value="<?php echo $producto['proveedor']; ?>" required><br>
+            <label for="idProveedor">ID del Proveedor:</label>
+            <input type="text" name="idProveedor" value="<?php echo $producto['idProveedor']; ?>" required><br>
+            <label for="stock">Stock:</label>
+            <input type="text" name="stock" value="<?php echo $producto['stock']; ?>" required><br>
+            <label for="precio">Precio:</label>
+            <input type="number" step="0.01" name="precio" value="<?php echo $producto['precio']; ?>" required><br>
+            <button type="submit">Guardar Cambios</button>
+        </form>
+    </div>
+<?php } ?>
 
     </div>
     <h2 id="anadirTitulo" style="display: none;">Añadir Producto</h2>
@@ -124,5 +127,6 @@ echo "</tr>";
         <button type="submit">Añadir Producto</button>
     </form>
     </div>
+    <script src="modificarProducto.js"></script>
 </body>
 </html>
